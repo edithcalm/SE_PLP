b05c03ecb0d68747d2363f902d1535eefbf617d4
edithcalm <edithkaranja04@gmail.com>
2025-04-23 19:07:14 +0300

    commit

--- python session/week 4 python/oop.py
+++ python session/week 4 python/oop.py
@@ -1,3 +1,8 @@
 print("wassp")
 if __name__ == "__main__":
     print("This is the main module")
+
+else:
+    print("This is not the main module")
+    print(__name__)
+    print("This is the main module")
