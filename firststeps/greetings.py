1e30f63b5648fbe6d3e73a3cf330017ece4d8863
edithcalm <edithkaranja04@gmail.com>
2025-05-23 15:57:58 +0300

    push

--- firststeps/greetings.py
+++ firststeps/greetings.py
@@ -11,3 +11,18 @@ def add_numbers (x,y):
     return x + y
 result = add_numbers (7,60)
 print(result)
+# Outputs: 67
+# Calling the function with different arguments 
+result = add_numbers (10,20)
+print(result)
+# Outputs: 30
+# Calling the function with different arguments
+result = add_numbers (100,200)
+
+print(result)
+# Outputs: 300
+# Calling the function with different arguments                         
+result = add_numbers (1000,2000)
+print(result)
+# Outputs: 3000 
+
